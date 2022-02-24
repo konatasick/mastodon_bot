647765bb18232772c2d8ec6a2e908b2768b0984a
root <root@vmi792614.contaboserver.net>
2022-02-24 11:11:22 +0800

    edit test of StandUp_bot

--- StandUp_bot.py
+++ StandUp_bot.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     id = "StandUp_bot"
     now = datetime.now()  # 获取当前datetime
     hour = int(now.strftime("%H"))
+    if hour > 12:
+        hour -= 12
     text = f"{hour}点了，起来站一站走一走吧！"
     records = post_toot(text, id)
